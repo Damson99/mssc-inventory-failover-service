68ee6a1f0e4002f0e1dac6225c09d64635fe70c4
DaMSoN <damian.wieczorek99@gmail.com>
2020-09-13 00:17:30 +0200

    initial project add. Closes #1

---
+++ src/main/java/mssc/beer/MsscInventoryFailoverServiceApplication.java
@@ -0,0 +1,14 @@
+package mssc.beer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscInventoryFailoverServiceApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscInventoryFailoverServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/mssc/beer/config/LocalDiscoveryConfig.java
@@ -0,0 +1,13 @@
+package mssc.beer.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@EnableDiscoveryClient
+@Configuration
+@Profile("local-discovery")
+public class LocalDiscoveryConfig
+{
+
+}
